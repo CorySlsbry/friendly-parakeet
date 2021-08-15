f39a8e7aeffe3ab18a98d0e9e0506744d8b04afd
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-08-14 22:31:08 -0500

    functions added

--- script.js
+++ script.js
@@ -1,33 +1,83 @@
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
-const arrayTypes = [{getLower}, {getUpper}, {getNumber}, {getSpecial}]
-console.log('arayTypes: ', arrayTypes);
-
+var passwordArray = [];
 // Prompt user for input
-
 var passwordLength = prompt("How many characters do you want in your password? Please choose between 8-128.")
-//Checks input
+//requests input from user
 if (passwordLength >= 8 && passwordLength <= 128)  {
-console.log(passwordLength)
-
-var getLower = confirm("Include lower case letters?")
-var getUpper = confirm("Include upper case letters?")
-var getNumber = confirm("Include numbers?")
-var getSpecial = confirm("Inlcude special characters?") 
-
-} else { (passwordLength < 8 || passwordLength > 128) 
-  alert("Length must be 8-128 characters");
+  var getLower = confirm("Include lower case letters?")
+  var getUpper = confirm("Include upper case letters?")
+  var getNumber = confirm("Include numbers?")
+  var getSpecial = confirm("Inlcude special characters?")
+  alert("Your options are saved. Click Generate Password.") 
+} 
+else {
+alert("Length must be 8-128 characters");
 }
-//Generates password character 
 
-  // Functions to generate random characters
-  randomFunction = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomUpper,
-    special: getRandomSpecial  
-  }; 
+
+
+   
+var randomFunction = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomUpper,
+  special: getRandomSpecial
+}
+
+
+  // Add event listener to generate button
+generateBtn.addEventListener("click", () => {
+  var length = parseInt(passwordLength);
+  var hasLower = getLower;
+  var hasUpper = getUpper;
+  var hasNumber = getNumber;
+  var hasSpecial = getSpecial;
+
+password.innerText = password(
+  hasLower,
+  hasUpper,
+  hasNumber,
+  hasSpecial,
+  length
+  )
   
+});
+    
+//   MOVED     MOVED    MOVED
+/*
+
+  console.log(typeArray);
+
+if(typeArray.length === 0) {
+  alert("You must choose at lease 1 option.");
+}*/
+
+
+  
+}
+
+function generatePassword(lower, upper, number, special, length) {
+  generatedPassword = "";
+  var typesCount = lower + upper + number + special;
+ // console.log("typesArr: ", typesArr);
+}
+for(var i = 0; i = passwordLength; i += typeArray) {
+  typeArray.forEach(type => {
+    const randomFunction = Object.keys(type) [0];
+    generatedPassword = "";
+    const typeArray = [{getLower}, {getUpper}, {getNumber}, {getSpecial}].filter
+    (item => Object.values(item) [0]);
+  
+    console.log(typeArray);
+  
+  if(typeArray.length === 0) {
+    alert("You must choose at lease 1 option.");
+  }
+  });
+
+// generate random charactors from charactor code
   function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
@@ -42,58 +92,18 @@ var getSpecial = confirm("Inlcude special characters?")
   
   function getRandomSpecial() {
     return String.fromCharCode(Math.floor(Math.random() *15 ) + 33);
+    
   }
 
 
- /*
-//Array
-let selectedChar = [getRandomLower || getRandomUpper || getRandomNumber || getRandomSpecial];
-let Length = [Number(passwordLength)];  
-
-  {
-    
-    if (getRandomLower) {
-      selectedChar += getRandomLower;
-    }
-
-    if (getRandomUpper) {
-      selectedChar += getRandomUpper;
-    }
-    
-    if (getRandomNumber) {
-      selectedChar += getRandomNumber;    
-    }
-
-    if (getRandomSpecial) {
-      selectedChar += getRandomSpecial;
-    }
-
-    console.log(selectedChar);
-  }
-*/
 // Write password to the #password input
 function writePassword() {
-  /* const length = +passwordLength.value;
-  const hasLower = lower.true;
-  const hasUpper = upper.true;
-  const hasNumber = number.true;
-  const hasSpecial = special.true; */
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-console.log(hasLower, hasNumber, hasSpecial, hasUpper)
-  for (var i = o; i < passwordLength; i++) {
-    result.innerText = characters(Math.floor(Math.random() * passwordLength));
-  }
-
   passwordText.value = password;
 
-
 }
-
-
+}
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
+// generateBtn.addEventListener("click", writePassword);
