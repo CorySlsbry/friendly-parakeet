f545cac9839c1794abd1bbbfd1bb8cce9b8ac820
CorySlsbry <87287656+CorySlsbry@users.noreply.github.com>
2021-08-24 11:23:11 -0500

    get password functionality fixed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var passwordArray = [];
 lower = getRandomLower()
 upper = getRandomUpper()
 number = getRandomNumber()
-special = getRandomSpecial()  
+special = getRandomSpecial()
 
 
 function getRandomLower() {
@@ -19,85 +19,66 @@ function getRandomUpper() {
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() *10 ) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSpecial() {
-  return String.fromCharCode(Math.floor(Math.random() *15 ) + 33);
-  
-} 
-
-
-// Prompt user for input
-var passwordLength = prompt("How many characters do you want in your password? Please choose between 8-128.")
-//requests input from user
-if (passwordLength >= 8 && passwordLength <= 128)  {
-  var getLower = confirm("Include lower case letters?")
-  var getUpper = confirm("Include upper case letters?")
-  var getNumber = confirm("Include numbers?")
-  var getSpecial = confirm("Inlcude special characters?")
-  alert("Your options are saved. Click Generate Password.") 
-} 
-else {
-alert("Length must be 8-128 characters");
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
 }
 
 
-  // Add event listener to generate button
+
+// Add event listener to generate button
 generateBtn.addEventListener("click", (element) => {
   element.preventDefault()
+  // Prompt user for input
+  var passwordLength = prompt("How many characters do you want in your password? Please choose between 8-128.")
+  //requests input from user
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    var getLower = confirm("Include lower case letters?")
+    var getUpper = confirm("Include upper case letters?")
+    var getNumber = confirm("Include numbers?")
+    var getSpecial = confirm("Inlcude special characters?")
+  }
+  else {
+    alert("Length must be 8-128 characters");
+  }
+
   const passwordLengthEl = parseInt(passwordLength)
-  const getLowerEl = getLower
-  const getUpperEl = getUpper
-  const getSpecialEl = getSpecial
-  const getNumberEl = getNumber
-  const password = generatePassword(passwordLengthEl, getLowerEl, getUpperEl, getSpecialEl, getNumberEl)
+  // const getLowerEl = getLower
+  // const getUpperEl = getUpper
+  // const getSpecialEl = getSpecial
+  // const getNumberEl = getNumber
+  const password = generatePassword(passwordLengthEl, getLower, getUpper, getSpecial, getNumber)
   passwordArea.innerText = password
-  console.log(password)
-   
+  
+
 })
 
 function generatePassword(passwordLengthEl, getLowerEl, getUpperEl, getSpecialEl, getNumberEl,) {
-  let generatedPassword = []
-    if (getLowerEl) generatedPassword = generatedPassword.concat(lower)
-    if (getUpperEl) generatedPassword = generatedPassword.concat(upper)
-    if (getNumberEl) generatedPassword = generatedPassword.concat(number)
-    if (getSpecialEl) generatedPassword = generatedPassword.concat(special)
-  
-  let passwordCharacters = []
-  for (let i = 0; i < parseInt(passwordLengthEl); i++) {
-    const characterList = generatedPassword[Math.floor(Math.random() * generatedPassword.length)]
-    passwordCharacters.push(String.fromCharCode(characterList))
-  }
-
-  return passwordCharacters.join('')
+  let generatedPassword = ""
+  for (let i = 0; i < parseInt(passwordLengthEl); i++)  {
+  // Picking a random number from 4 options each one representing character in password
+    var pick = Math.floor(Math.random() * 4) + 1
+  // Create if statements
+      // if lower true
+   if (getLowerEl && pick === 1) {
+      var x = getRandomLower()
+      generatedPassword += x
+    } else if (getUpperEl && pick === 2) {
+        var x = getRandomUpper()
+        generatedPassword += x
+      
+    } else if (getSpecialEl && pick === 3) {
+      var x = getRandomSpecial()
+      generatedPassword += x
+    
+  } else if (getNumberEl && pick === 4) {
+    var x = getRandomNumber()
+    generatedPassword += x
   
 }
-
-/*function passwordArray() {
-  const array = []
-  for (let i = 0; i <= passwordLengthEl; i++) {
-    array.push(i)
-  }
-  return array  
-}*/
-
-
-
-
-
-
   
- 
-
-
-  
-
-/*password.innerText = passwordArray(
-  getLower,
-  getUpper,
-  getNumber,
-  getSpecial,
-  length) */
-//}
-
+}
+}
+console.log(generatedPassword);
